7a30753bd7ab13848d810825e1f0b15d3cfcb2ee
Aarik Guy <aarik15dg@gmail.com>
2025-02-12 16:12:15 -0700

    started task 2, still in progress

--- src/lab.c
+++ src/lab.c
@@ -1,1 +1,24 @@
-/**Update this file with the starter code**/
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int
+main (int argc, char **argv)
+{
+
+  while ((c = getopt (argc, argv, "v:")) != -1)
+    switch (c)
+      {
+
+      case 'v':
+
+      fprintf(lab_VERSION_MAJOR)
+        break;
+      
+      default:
+        abort ();
+      }
+
+
+}
