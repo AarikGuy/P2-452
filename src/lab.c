1704fc31975253d9b9eaa8e010009516a9b7299b
Aarik Guy <aarik15dg@gmail.com>
2025-02-14 22:46:58 -0700

    task 2 complete

--- src/lab.c
+++ src/lab.c
@@ -2,23 +2,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include "lab.h"
 
-int
-main (int argc, char **argv)
+int main (int argc, char **argv)
 {
+  int c;
 
-  while ((c = getopt (argc, argv, "v:")) != -1)
-    switch (c)
-      {
-
-      case 'v':
-
-      fprintf(lab_VERSION_MAJOR)
-        break;
-      
-      default:
-        abort ();
+  while ((c = getopt(argc, argv, "v:")) != -1)
+      switch (c){
+        case 'v':
+          printf("Shell Version: %d.%d\n", lab_VERSION_MAJOR, lab_VERSION_MINOR);
+          exit(0);
+        default:
+          abort ();
       }
-
-
+    return 0;
 }
