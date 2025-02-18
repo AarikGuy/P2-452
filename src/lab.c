115e2393cc5d2f7f66dc3fa7f2b421e1871c2956
Aarik Guy <aarik15dg@gmail.com>
2025-02-17 23:24:53 -0700

    updated command line parsing

--- src/lab.c
+++ src/lab.c
@@ -2,19 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 #include "lab.h"
+
+struct shell sh;
 
 int main (int argc, char **argv)
 {
   int c;
 
+  //Parses Command line args
   while ((c = getopt(argc, argv, "v:")) != -1)
       switch (c){
         case 'v':
           printf("Shell Version: %d.%d\n", lab_VERSION_MAJOR, lab_VERSION_MINOR);
           exit(0);
         default:
-          abort ();
+          fprintf(stderr, "Usage: %s [-v]\n", argv[0]);
+          exit(EXIT_FAILURE);
       }
     return 0;
 }
