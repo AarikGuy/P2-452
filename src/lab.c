9a0e49f1f47caf82e575ceb17d67096aaaa9ea27
Aarik Guy <aarik15dg@gmail.com>
2025-02-17 23:43:55 -0700

    implemented get prompt and put in the skeleton of the lab.h's functions.

--- src/lab.c
+++ src/lab.c
@@ -24,3 +24,41 @@ int main (int argc, char **argv)
       }
     return 0;
 }
+
+// Displays the prompt
+char *get_prompt(const char *env) {
+  char *prompt = getenv(env);
+  return prompt ? prompt : "myshell> ";
+}
+
+int change_dir(char **dir){
+
+}
+
+char **cmd_parse(char const *line){
+
+}
+
+void cmd_free(char ** line){
+
+}
+
+char *trim_white(char *line){
+
+}
+
+bool do_builtin(struct shell *sh, char **argv){
+
+}
+
+void sh_init(struct shell *sh){
+
+}
+
+void sh_destroy(struct shell *sh){
+
+}
+
+void parse_args(int argc, char **argv){
+  
+}
